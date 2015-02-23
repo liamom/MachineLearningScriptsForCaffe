dcb7a3018a16815d8d8f85af66d17f12058706b2
liamom <lolivermallory123@pointloma.edu>
2015-02-22 22:20:39 -0800

    main

---
+++ convert_binary.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+import glob
+
+for filename in glob.glob("train/Seq01/Seq01/imagesjpg/*.jpg"):#specify your own path
+	img = cv2.imread(filename)
+	img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY )
+	cv2.imwrite( filename, img )
+
+
