6ba791eeef9cee6785716b7ce0592ca08ae2a7f6
James Morar <jmorar567@pointloma.edu>
2015-03-05 21:34:35 -0800

    pixel function

--- is_white_space.py
+++ is_white_space.py
@@ -7,19 +7,14 @@ import sys
 from numpy import array
 from PIL import Image
 
-def isWhite(img1, img2, pixelNum):
-	white = 0
-	im = Image.open(img1)
-	#arr = array(im) #this is a numpy array
-	count = 0
-	for pixel in iter(im.getdata()):
-		count++
-		if count == pixelNum:
-			if pixel[pixelNum] == #(255,255,255):
-				white = 1
-			else:
-				white = 0
-			break
-			
-
-        	
+def isWhite(maskImg, x, y):
+	
+	im = Image.open(maskImg)
+	
+	pix = im.load()
+	if pix[x, y] == (255, 255, 255):
+		return 1
+	else:
+		return 0
+		
+	
